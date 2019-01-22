16594b986ff1de614843be05fbd9d13f46351923
BuildTools <unconfigured@null.spigotmc.org>
2019-01-22 12:38:50 -0500

    Fixed errors, cleaned code - new header file for structs

---
+++ tiltControlTest/structs.h
@@ -0,0 +1,16 @@
+struct SensorValue {
+    float x;
+    float y;
+    float z;
+};
+
+struct JoyStickValue {
+    int x;
+    int y;
+    float angle;
+    float distance;
+    String eightdirection;
+    String fourdirection;
+};
+
+
